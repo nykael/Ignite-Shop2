6e1a9a91f7fe9dc3c3b5b936cd0dc339cb917a8f
nykael <nykaelccardial@gmail.com>
2023-08-16 09:30:04 -0300

    configNextJs

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '10px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.9)'
+  }
+})
+
+export default function Home() {
+  return (
+    <Button><span>Hello</span>world</Button>
+  )
+}
