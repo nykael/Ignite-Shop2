c90a65799ea5f66b5ac0729052fed36cfa58eb66
nykael <nykaelccardial@gmail.com>
2023-08-16 15:29:27 -0300

    Shirts card

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
+import LogoImg from'../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <Container>
+      <Header>
+        <Image src={LogoImg.src} alt=""  width={200} height={130}/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,39 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '10px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.9)'
-  }
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <Button><span>Hello</span>world</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </Product>
+
+      {/* <Product>
+        <Image src={camiseta3} width={520} height={520} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,98</span>
+        </footer>
+      </Product> */}
+    </HomeContainer>
   )
 }
